c8bb400345bfe6805f418886457f2b172afa5090
Carl Dawson <carl@invisible-gorilla.com>
2018-10-19 05:18:44 -0400

    Added divisions controller

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ require('./db');
 
 const app = express();
 
+app.use(express.json());
 app.use('/', router);
 
 app.listen(config.port);
--- server/controllers/divisions.js
+++ server/controllers/divisions.js
@@ -3,10 +3,25 @@ const router = require('express').Router();
 const {Division} = require('../models/division');
 
 router.get('/', async (req, res) => {
+  try {
+    const divisions = await Division.find();
+    res.send(divisions);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-  const divisions = await Division.find();
+router.post('/', async (req, res) => {
+  const division = new Division({
+    name: req.body.name
+  });
 
-  res.send(divisions);
+  try {
+    await division.save();
+    res.send(division);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 module.exports = router;
