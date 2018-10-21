90b3f45ff3d834d7d5775d411ce238672c33ac79
Carl Dawson <carl@invisible-gorilla.com>
2018-10-21 10:03:32 -0400

    Added cors middleware

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const config = require('./config/config');
 const router = require('./config/router');
@@ -7,7 +8,9 @@ require('./config/db');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/', router);
 
 app.listen(config.port);
