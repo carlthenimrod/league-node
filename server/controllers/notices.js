68c238814acf27bd8f8c6f39b14c87a27a021242
Carl Dawson <carl@invisible-gorilla.com>
2018-12-18 17:23:18 -0500

    Added notice remove endpoint

--- server/controllers/notices.js
+++ server/controllers/notices.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const {ObjectID} = require('mongodb');
 
 const {Notice} = require('../models/notice');
 
 router.get('/', async (req, res) => {
   try {
-    const notices = await Notice.find().populate('item');
+    const notices = await Notice.find().sort('-createdAt').populate('item');
     res.send(notices);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  try {
+    await Notice.findByIdAndDelete(id);
+    res.send();
   } catch (e) {
     res.status(400).send(e);
   }
