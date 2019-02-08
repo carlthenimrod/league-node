d1f861eff4bf3c9a5d97f8b2cc15f7813e6c599e
Carl Dawson <carl@invisible-gorilla.com>
2019-02-08 06:05:28 -0500

    Removed place location endpoints, no longer need

--- server/controllers/places.js
+++ server/controllers/places.js
@@ -15,11 +15,12 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const {
     name,
-    address
+    address,
+    locations
   } = req.body;
 
   try {
-    const place = new Place({ name, address });
+    const place = new Place({ name, address, locations });
     await place.save();
     res.send(place);
   } catch (e) {
@@ -48,7 +49,8 @@ router.put('/:id', async (req, res) => {
   const id = req.params.id;
   const {
     name,
-    address
+    address,
+    locations
   } = req.body;
 
 
@@ -69,6 +71,7 @@ router.put('/:id', async (req, res) => {
 
     place.name = name;
     place.address = address;
+    place.locations = locations;
     await place.save();
     res.send(place);
   } catch (e) {
@@ -87,94 +90,6 @@ router.delete('/:id', async (req, res) => {
     }
 
     await Place.findByIdAndDelete(id);
-    res.send();
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.post('/:id/locations', async (req, res) => {
-  const id = req.params.id;
-  const {name} = req.body;
-
-  try {
-    if (!ObjectID.isValid(id)) {
-      const err = new Error('Invalid ID');
-      err.status = 404;
-      throw err;
-    }
-
-    const place = await Place.findById(id);
-
-    if (!place) {
-      const err = new Error('Place not found');
-      err.status = 404;
-      throw err;
-    }
-
-    place.locations.push({ name });
-    await place.save();
-    res.send(place);
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.put('/:id/locations/:locationId', async (req, res) => {
-  const {id, locationId} = req.params;
-  const {name} = req.body;
-
-  try {
-    if (!ObjectID.isValid(id) || !ObjectID.isValid(locationId)) {
-      const err = new Error('Invalid ID');
-      err.status = 404;
-      throw err;
-    }
-
-    const place = await Place.findById(id);
-
-    if (!place) {
-      const err = new Error('Place not found');
-      err.status = 404;
-      throw err;
-    }
-
-    const location = place.locations.id(locationId);
-
-    if (!location) {
-      const err = new Error('Location not found');
-      err.status = 404;
-      throw err;
-    }
-
-    location.name = name;
-    await place.save();
-    res.send(place);
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.delete('/:id/locations/:locationId', async (req, res) => {
-  const {id, locationId} = req.params;
-
-  try {
-    if (!ObjectID.isValid(id) || !ObjectID.isValid(locationId)) {
-      const err = new Error('Invalid ID');
-      err.status = 404;
-      throw err;
-    }
-
-    const place = await Place.findById(id);
-
-    if (!place) {
-      const err = new Error('Place not found');
-      err.status = 404;
-      throw err;
-    }
-
-    place.locations.id(locationId).remove();
-    await place.save();
     res.send();
   } catch (e) {
     next(e);
