ea6920a9a43ce62912c5c802b0342bb2b632176c
Carl Dawson <carl@invisible-gorilla.com>
2019-06-02 20:59:57 -0400

    Added verify email test function

--- server/config/email.js
+++ server/config/email.js
@@ -38,7 +38,14 @@ const mailer = {
     const transporter = await getTransporter(),
           to = (typeof options === 'string') ? options : options.to;
 
-          console.log(transporter);
+    // verify connection configuration
+    transporter.verify(function(error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
 
     if (transporter) {
       try {
