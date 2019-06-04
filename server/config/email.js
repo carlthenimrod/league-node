8e95c3903ca5d59e8cfe06a81b316f6871e3b648
Carl Dawson <carl@invisible-gorilla.com>
2019-06-04 02:12:48 -0400

    Disabled creating test emails temp

--- server/config/email.js
+++ server/config/email.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
 const sgMail = require('@sendgrid/mail');
-const open = require('open');
-const temp = require('temp').track();
+// const open = require('open');
+// const temp = require('temp').track();
 
 const config = require('./config');
 
@@ -42,21 +42,21 @@ const send = async (template, options, data = {}) => {
       // send email
       sgMail.send(msg);
     } else {
-      // create temporary file
-      temp.open({ suffix: '.html' }, async (e, info) => {
-        if (e) throw(e);
+      // // create temporary file
+      // temp.open({ suffix: '.html' }, async (e, info) => {
+      //   if (e) throw(e);
         
-        // write to file
-        fs.write(info.fd, html, (err) => {
-          if (err) { throw(err); }
-        });
+      //   // write to file
+      //   fs.write(info.fd, html, (err) => {
+      //     if (err) { throw(err); }
+      //   });
 
-        // take file when done, open in chrome
-        fs.close(info.fd, (err) => {
-          if (err) { throw(err); }
-          open(info.path, { app: 'chrome' });
-        })
-      });
+      //   // take file when done, open in chrome
+      //   fs.close(info.fd, (err) => {
+      //     if (err) { throw(err); }
+      //     open(info.path, { app: 'chrome' });
+      //   })
+      // });
     }
   } catch (e) {
     console.log(e.toString());
