8bc2e7fc97f54565f9cdc9c92b4d35a1e785b93f
Carl Dawson <carl@invisible-gorilla.com>
2019-06-12 15:19:44 -0400

    removed console log

--- server/app.js
+++ server/app.js
@@ -15,7 +15,5 @@ app.use('/public', express.static(path.join('server/public')));
 
 app.use('/', router);
 
-console.log('test');
-
 const server = app.listen(config.port);
 require('./config/socket')(server);
